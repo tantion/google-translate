0bebfd6b3256b3db59c6a28f0ca81c81813707f2
tantion <hillcom@qq.com>
2014-07-31 22:42:10 +0800

    Fixed content-security-policy

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -34,6 +34,33 @@ chrome.webRequest.onHeadersReceived.addListener(function(info) {
 },
 ['blocking', 'responseHeaders']);
 
+// add Content-Security-Policy frame-src for some domain
+chrome.webRequest.onHeadersReceived.addListener(function(info) {
+    var headers = info.responseHeaders;
+    var directive = 'translate.google.com translate.google.com.hk';
+
+    for (var i=headers.length-1; i>=0; --i) {
+        var header = headers[i];
+        var name = header.name;
+        var value = header.value;
+        if (name.match(/Content-Security-Policy/i)) {
+            if (value.match(/frame-src ([^;]+)/)) {
+                header.value = value.replace(/(frame-src [^;]+)/, '$1 ' + directive);
+            } else {
+                header.value = value + '; frame-src ' + directive;
+            }
+        }
+    }
+
+    return {responseHeaders: headers};
+}, {
+    urls: [
+        '<all_urls>'
+    ],
+    types: ['main_frame']
+},
+['blocking', 'responseHeaders']);
+
 chrome.runtime.onMessage.addListener(function (message, sender, response) {
     var action = message.action;
     var msg = message.message;
